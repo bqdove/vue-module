af790242e1fa5e127f6fbad56483c53e6e415467
twilroad <269044570@qq.com>
2017-09-20 20:25:09 +0800

    fix module definition

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -14,7 +14,7 @@ module.exports = {
     output: {
         path: config.build.assetsRoot,
         filename: utils.assetsPath('js/module.min.js'),
-        library: 'notadd/content',
+        library: 'notadd/module',
         libraryTarget: "umd",
         publicPath: process.env.NODE_ENV === 'production'
             ? config.build.assetsPublicPath
